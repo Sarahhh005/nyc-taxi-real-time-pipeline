aa1caf1009acc12340d2f31c7904c4fcc1438cfa
EyadWaleed06 <eyad.waleed.f@gmail.com>
2026-08-18 10:33:05 +0300

    Create Initial.sql
    Added the initial ClickHouse setup for the NYC Taxi real-time analytics pipeline.
    
    This includes creating the "NYC_TAXI" database and the "taxi_trips" table using the "MergeTree" engine. The table contains the main trip information such as pickup and drop-off times, passenger count, trip distance, location IDs, fare amount, tip amount, and total amount.
    The schema is designed to store processed taxi data coming from Spark Streaming and support fast analytical queries in ClickHouse.

---
+++ clickhouse/Initial.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS NYC_TAXI;
+CREATE TABLE IF NOT EXISTS NYC_TAXI.taxi_trips
+(   pickup_datetime DateTime,
+    dropoff_datetime DateTime,
+    passenger_count UInt8,
+    trip_distance Float32,
+    pickup_location_id UInt16,
+    dropoff_location_id UInt16,
+    fare_amount Float32,
+    tip_amount Float32,
+    total_amount Float32)
+ENGINE = MergeTree()
+ORDER BY pickup_datetime;
