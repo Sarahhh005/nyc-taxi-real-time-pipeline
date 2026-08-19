1ed480677137b254cd3e3bcb83fa209c7e701415
EyadWaleed06 <eyad.waleed.f@gmail.com>
2026-08-19 22:38:45 +0300

    initial.sql

--- clickhouse/initial.sql
+++ clickhouse/initial.sql
@@ -1,14 +1,15 @@
 CREATE DATABASE IF NOT EXISTS NYC_TAXI;
 CREATE TABLE IF NOT EXISTS NYC_TAXI.taxi_trips
-(   pickup_datetime DateTime,
+(
+    pickup_datetime DateTime,
     dropoff_datetime DateTime,
-    passenger_count UInt8,
+    passenger_count Int32,
     trip_distance Float32,
-    pickup_location_id UInt16,
-    dropoff_location_id UInt16,
+    pickup_location_id Int32,
+    dropoff_location_id Int32,
     fare_amount Float32,
     tip_amount Float32,
     total_amount Float32,
-    payment_type UInt8)
+    payment_type Int32)
 ENGINE = MergeTree()
 ORDER BY pickup_datetime;
